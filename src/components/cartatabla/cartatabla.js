dae0d251579ff5633c27f439fe0cc5100fc8fa85
Laszlo-Caballero <laszlocaballero@gmail.com>
2023-03-02 18:08:35 -0500

    updated

--- src/components/cartatabla/cartatabla.js
+++ src/components/cartatabla/cartatabla.js
@@ -1,24 +1,41 @@
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./cartatabla.css";
-function cartatabla() {
+
+function Cartatabla() {
+  const [hovered, sethover] = useState(false);
+
+  const onhover = () => {
+    sethover(true);
+    console.log(hovered);
+  };
+  const leavehover = () => {
+    sethover(false);
+    console.log(hovered);
+  };
   return (
     <div
+      onMouseEnter={onhover}
+      onMouseLeave={leavehover}
       className="carta"
-      style={{ backgroundColor: "rgba(253, 128, 208, 0.4)" }}>
-      <img className="cover" alt="cover" src={require(`./mvv 2_after.jpg`)} />
-      <div
-        className="cartaRecursos"
-        style={{ backgroundColor: "rgba(25, 0, 16, 0.7)" }}>
+      style={{ backgroundColor: "#252525" }}>
+      <div className="imagenesCover">
+        <img className="cover" alt="cover" src={require(`./mvv 2_after.jpg`)} />
+        <Link to={"mvv2"}>
+          <img
+            className="iconoCover"
+            alt="play"
+            src={require("./play.png")}
+            style={{ display: hovered ? "" : "none" }}
+          />
+        </Link>
+      </div>
+      <div className="cartaRecursos">
         <p className="tituloCover"> Gira Me Veras Volver #2</p>
-        <div className="botonCoverContenedor">
-          <Link to={"/tabs/mvv2"}>
-            <button className="botonCover">Ver tabs -{">"} </button>
-          </Link>
-        </div>
+        <p className="descripcionCover">2008 - Soda Stereo</p>
       </div>
     </div>
   );
 }
 
-export default cartatabla;
+export default Cartatabla;
--- src/pages/recursos/recursos.js
+++ src/pages/recursos/recursos.js
@@ -1,8 +1,18 @@
 import Cartatabla from "../../components/cartatabla/cartatabla";
 import "./recursos.css";
 function recursos() {
+  const datosBD = [
+    {
+      titulo: "Gira Me Veras Volver #2",
+      descripcion: "",
+    },
+  ];
+
   return (
     <div className="main">
+      <Cartatabla />
+      <Cartatabla />
+      <Cartatabla />
       <Cartatabla />
       <Cartatabla />
       <Cartatabla />
